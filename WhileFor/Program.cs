049deb29967499425abef2254697e7bc7f41df1d
VanRar <vitalikf7@gmail.com>
2020-06-27 17:06:49 +0300

    Добавьте файлы проекта.

---
+++ WhileFor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WhileFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
